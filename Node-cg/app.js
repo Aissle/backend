471381322cdee00dc39cec75192b51db01edd988
Idris Aminat <idris.oaminat@gmail.com>
2024-10-10 21:48:50 +0100

    integrated body-parser

--- Node-cg/app.js
+++ Node-cg/app.js
@@ -1,16 +1,38 @@
 const http = require('http');
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware!');
-    next(); //allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
 });
 
-app.use((req,res,next) => {
-    console.log('in another middleware');
-    res.send('<h1>Hello from Express!</h1>');
+app.use('/add-product', (req, res, next) => {
+    console.log('in add product middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    //next(); //allows the request to continue to the next middleware in line
+});
+
+app.use('/product', (req, res, next) => {
+    console.log('in product middleware');
+    console.log(req.body);
+    res.redirect('/');
+    //next(); //allows the request to continue to the next middleware in line
+});
+
+app.use('/', (req,res,next) => {
+    console.log('in home middleware');
+    res.send('<h1>home Page!</h1>');
 });
 app.listen(4000);
+
+//node.js
+//npm
+//nodemon
+//express
+//body-parser
