38c3e68c1fd0eac97930a82d01b9fd8c9bb539a7
Idris Aminat <idris.oaminat@gmail.com>
2024-10-10 12:42:36 +0100

    integrated express into code

--- Node-cg/app.js
+++ Node-cg/app.js
@@ -3,6 +3,14 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-const server = http.createServer(app);
 
-server.listen(4000);
+app.use((req, res, next) => {
+    console.log('in the middleware!');
+    next(); //allows the request to continue to the next middleware in line
+});
+
+app.use((req,res,next) => {
+    console.log('in another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+app.listen(4000);
