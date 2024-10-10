3908d2aff4b1b8bfc2985932794fb576612a3ff7
Idris Aminat <idris.oaminat@gmail.com>
2024-10-10 05:40:17 +0100

    copied routes and app to practice folder

--- Practice/app.js
+++ Practice/app.js
@@ -1,12 +1,7 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</tittle></head>');
-    res.write('<body><h1>Hello from Node.js server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const routes = require('./routes');
 
-server.listen(3000);
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
+
+server.listen(4000);
