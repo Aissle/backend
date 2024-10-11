8850e1ea63aea1f72bfce4522bf7d13c91efeb8a
Idris Aminat <idris.oaminat@gmail.com>
2024-10-11 02:29:55 +0100

    added a 'not found' page and adjusted the url for admin pages

--- Node-cg/app.js
+++ Node-cg/app.js
@@ -10,15 +10,17 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    //console.log('This always runs!');
-    next();
-});
+// app.use('/', (req, res, next) => {
+//     //console.log('This always runs!');
+//     next();
+// });
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-app.listen(4000);
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+app.listen(4000 );
 
 //node.js
 //npm
--- Node-cg/routes/admin.js
+++ Node-cg/routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     //console.log('in add product middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
     //next(); //allows the request to continue to the next middleware in line
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     //console.log('in product middleware');
     console.log(req.body);
     res.redirect('/');
