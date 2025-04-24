dc5a8b512ea8f4b87696a5dddd15989e863cffe9
Idris Aminat <idris.oaminat@gmail.com>
2025-04-24 21:47:18 +0100

    define product model

--- Node-cg/models/product.js
+++ Node-cg/models/product.js
@@ -1,33 +1,22 @@
-const db = require('../util/database');
+const Sequilize = require('sequelize');
+const sequelize = require('.../util/database');
 
-const Cart = require('./cart');
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Product = sequelize.define('product', {
+    id: {
+        typr: Sequilize.INTEGER,
+        autoincrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: sequelize.STRING,
+        allowNull: false
     }
+});
 
-    save() {
-        return db.execute(
-            'INSERT INTO products (title, price, imageurl, description) VALUES (?, ?, ?, ?)',
-            [this.title, this.price, this.imageUrl, this.description]
-        );
-    }
-
-    static deleteById(id) {
-      
-    }
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products');
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-    }
-
-}
+module.exports = Product;
