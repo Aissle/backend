7e8419f09c783223a04e478fea0587abefc41665
Idris Aminat <idris.oaminat@gmail.com>
2025-04-24 21:16:27 +0100

    connect sequelize to database

--- Node-cg/util/database.js
+++ Node-cg/util/database.js
@@ -1,16 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-// Create a connection pool
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Aminat',
-    database: 'node-complete',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const sequelize = new Sequelize('node-complete', 'root', 'Aminat', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-// Export the pool for use in other parts of the application
-module.exports = pool.promise();
-
+module.exports = sequelize;
