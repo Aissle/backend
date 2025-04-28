e73915a10589d134bc520d20a0360b389bdaabf2
Idris Aminat <idris.oaminat@gmail.com>
2025-04-28 05:16:33 +0100

    add one to many relationship

--- Node-cg/app.js
+++ Node-cg/app.js
@@ -11,8 +11,11 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
+
 const errController = require('./controllers/errController');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: false}));        
 app.use(express.static(path.join(rootDir, 'public')));
@@ -22,7 +25,11 @@ app.use(shopRoutes);
 
 app.use(errController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+    .sync({force: true})
     .then(result => {
         //console.log(result);
         app.listen(4000 );
