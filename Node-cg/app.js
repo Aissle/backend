d8dbc78f6ab1afc81df77897be540cf55fce5b8d
Idris Aminat <idris.oaminat@gmail.com>
2025-04-28 16:41:43 +0100

    18. One-To-Many & Many-To-Many Relations

--- Node-cg/app.js
+++ Node-cg/app.js
@@ -16,6 +16,8 @@ const errController = require('./controllers/errController');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use(bodyParser.urlencoded({extended: false}));        
 app.use(express.static(path.join(rootDir, 'public')));
@@ -36,10 +38,14 @@ app.use(errController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-    //.sync({force: true})
-    .sync()
+    .sync({force: true})
+    //sync()
     .then(result => {
         //console.log(result);
         return User.findByPk(1);
