9074184954814c659dd4283eabfa55d967249f62
Idris Aminat <idris.oaminat@gmail.com>
2025-05-10 23:19:20 +0100

    connect Mongodb
    successfully connected mongodb

--- Node-cg/util/database.js
+++ Node-cg/util/database.js
@@ -4,11 +4,13 @@ const MongoClient = mongodb.MongoClient;
 const mongoConnect = callback => {
     MongoClient.connect(
         // 'mongodb+srv://jane:jane123@cluster0.18p7zv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-        'mongodb+srv://jane:jane123@cluster0.18p7zv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        //'mongodb+srv://jane:jane123@cluster0.18p7zv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        'mongodb+srv://Jane:Jane123@cluster0.18p7zv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
     )
-        .then(result => {
+        .then(client => {
             console.log('connected!');
+            callback(client);
         })
         .catch(err => {
             console.log(err);
