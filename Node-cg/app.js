3fa64be9602e1cd3c20b055863072ebab6a7acc4
Idris Aminat <idris.oaminat@gmail.com>
2025-05-12 23:23:27 +0100

    Update app.js

--- Node-cg/app.js
+++ Node-cg/app.js
@@ -11,6 +11,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+
 // const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
